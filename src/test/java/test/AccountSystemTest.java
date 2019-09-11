79bf27490a348602a9c8bbabe63ddb9e5ebcc651
outellx <outellx@live.com>
2019-09-11 13:50:35 +0800

    Update AccountSystemTest.java

--- src/test/java/test/AccountSystemTest.java
+++ src/test/java/test/AccountSystemTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertTrue;
 public class AccountSystemTest {
     @Test
     public void name() {
-        assertTrue(true);
+//        assertTrue(true);
+        assertTrue(false);
     }
 }
